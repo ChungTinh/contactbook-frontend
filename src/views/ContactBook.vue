9fe2dbbcadce81947018decc84bcd7bfe03c42a2
ChungTinh <tinhb2306643@student.ctu.edu.vn>
2026-07-23 13:28:38 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -30,6 +30,16 @@
           <i class="fas fa-address-card"></i>
         </h4>
         <ContactCard :contact="activeContact" />
+        <router-link
+          :to="{
+            name: 'contact.edit',
+            params: { id: activeContact._id },
+          }"
+        >
+          <span class="mt-2 badge badge-warning">
+            <i class="fas fa-edit"></i> Hiệu chỉnh
+          </span>
+        </router-link>
       </div>
     </div>
   </div>
@@ -73,7 +83,7 @@ export default {
     filteredContacts() {
       if (!this.searchText) return this.contacts;
       return this.contacts.filter((_contact, index) =>
-        this.contactStrings[index].includes(this.searchText)
+        this.contactStrings[index].includes(this.searchText),
       );
     },
     activeContact() {
